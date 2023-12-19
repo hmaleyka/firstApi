faafacb75e29d19f024cf05f49a49fc9e271fbb2
hmaleyka <maleykaheybatova1011@gmail.com>
2023-12-19 18:41:40 +0400

    repository pattern

--- firstAPI/Program.cs
+++ firstAPI/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
